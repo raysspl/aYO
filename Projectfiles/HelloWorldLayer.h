53741892ffe75dfffe90aa9a7a177530e801236a
raydipasupil@gmail.com <raydipasupil@gmail.com>
2013-12-15 18:04:02 -0800

    Mockup of GameOfLife

--- Projectfiles/HelloWorldLayer.h
+++ Projectfiles/HelloWorldLayer.h
@@ -1,21 +1,23 @@
 /*
  * Kobold2D™ --- http://www.kobold2d.org
  *
- * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Copyright (c) 2010-2011 Steffen Itterheim.
  * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
  */
 
-#import "cocos2d.h"
+#import "kobold2d.h"
 
-@interface HelloWorldLayer : CCLayer
-{
-	NSString* helloWorldString;
-	NSString* helloWorldFontName;
-	int helloWorldFontSize;
-}
-
-@property (nonatomic, copy) NSString* helloWorldString;
-@property (nonatomic, copy) NSString* helloWorldFontName;
-@property (nonatomic) int helloWorldFontSize;
+@interface GameOfLifeLayer : CCLayer
+-(id) init;
+-(void) draw;
+-(void) update: (ccTime) delta;
+-(void) nextFrame;
+-(void) resetArrays;
+-(int) prevRow: (int) row;
+-(int) nextRow: (int) row;
+-(int) prevCol: (int) col;
+-(int) nextCol: (int) col;
+-(void) countNeighbors;
+-(void) updateGrid;
 
 @end
